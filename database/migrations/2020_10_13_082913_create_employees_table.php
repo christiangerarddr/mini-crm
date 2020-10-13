3f1c0d21b3b3e171eb2a70d88e13083951479e85
christian gerard delos reyes <christiangerarddelosreyes@gmail.com>
2020-10-13 16:35:22 +0800

    add foreign key to employees

--- database/migrations/2020_10_13_082913_create_employees_table.php
+++ database/migrations/2020_10_13_082913_create_employees_table.php
@@ -15,6 +15,10 @@ class CreateEmployeesTable extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->integer('phone');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2020_10_13_083230_add_foreign_keys_to_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKeysToEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->unsignedBigInteger('company_id')->nullable();
+
+            $table->foreign('company_id')
+                ->references('id')
+      			->on('companies')
+      			->onDelete('restrict')
+      			->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            //
+        });
+    }
+}
